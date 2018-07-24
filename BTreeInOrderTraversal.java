5bab3816cabf35da5f086c55d21bbfe3d76563ab
Abhishek Sharma <abhisheks@berkeley.edu>
2018-07-23 22:01:50 -0700

    LeetCode #94 Binary Tree In Order Traversal

---
+++ BTreeInOrderTraversal.java
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root == None:
+            return []
+        cache = []
+        def pit(root):
+            try:
+                pit(root.left)
+            except AttributeError:
+                jsfadf = 0
+            
+            cache.append(root.val)
+            
+            try:
+                pit(root.right)
+            except AttributeError:
+                jsfadf = 0
+        
+        pit(root) 
+        return cache
