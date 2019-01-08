6da89e552159ed72175d8825a5979949cd498b99
Abhishek Sharma <abhisheks@berkeley.edu>
2019-01-07 21:39:00 -0800

    LeetCode #15 3Sum

---
+++ 3Sum.java
@@ -0,0 +1,23 @@
+import java.util.Arrays; 
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> answer = new ArrayList<List<Integer>>();
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i == 0 || (i>0 && nums[i] != nums[i-1])) {
+                int j = i+1, k = nums.length-1;
+                while (j < k) {
+                    int tsum = nums[i] + nums[j] + nums[k];
+                    if (tsum == 0) {
+                        answer.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        while (j < k && nums[j] == nums[j+1]) j++;
+                        while (j < k && nums[k] == nums[k-1]) k--;
+                        k--; j++;
+                    } else if (tsum < 0) j++;
+                    else k--;
+                }
+            }
+        }
+        return answer;        
+    }
+}
