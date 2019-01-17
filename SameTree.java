40a56335597fa86036289559693741e56e397570
Abhishek Sharma <abhisheks@berkeley.edu>
2019-01-17 15:53:59 -0800

    LeetCode #100 Same Tree

---
+++ SameTree.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p != null && q != null) {
+            return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return (p == null && q == null);
+    }
+}
