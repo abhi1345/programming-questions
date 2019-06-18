a991991974892348bf3d56f84ffaabd293506046
Abhishek Sharma <abhisheks@berkeley.edu>
2019-06-18 15:05:37 -0700

    LeetCode #709 To Lower Case

---
+++ toLowerCase.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for (int i = 0; i < str.size(); i++) {
+            if (str[i] <= 90 && str[i] >= 65) {
+                str[i] += 32;
+            }
+        }
+        return str;
+    }
+};
