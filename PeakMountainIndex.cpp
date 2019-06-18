bc4e4271679784ec9ca374b5784093ba190e2713
Abhishek Sharma <abhisheks@berkeley.edu>
2019-06-18 15:11:10 -0700

    LeetCode #852 Peak Index in a Mountain Array

---
+++ PeakMountainIndex.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+        for (int i = 1; i < A.size(); i++) {
+            if (A[i] > A[i+1]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+};
