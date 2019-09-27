d8e68c59a0d58f3c1dba8d4be52f792c2447a824
Abhishek Sharma <abhisheks@berkeley.edu>
2019-09-26 19:09:11 -0700

    LeetCode #191 Number of 1 Bits

---
+++ HammingWeight.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ans = 0
+        while n:
+            ans += n % 2
+            n = n // 2
+        return ans
